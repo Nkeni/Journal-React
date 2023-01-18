eacd5783ff6bcd88f0bec8d269f7b4870b79e7b9
Agatha A Msola <agatha@aims.ac.tz>
2023-01-18 16:00:01 +0100

    *made some changes

--- my-journal/src/App.jsx
+++ my-journal/src/App.jsx
@@ -1,30 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.scss';
-import Header from './components/Header/Header.jsx';
-import Home from './components/Home/Home.jsx';
-import Journal from './components/Journal/Journal.jsx';
-import Footer from './components/Footer/Footer.jsx';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
-import Edit from './components/Edit/Edit.jsx';
-import NotFound from './components/NotFound/NotFound.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.scss";
+import Header from "./components/Header/Header.jsx";
+import Home from "./components/Home/Home.jsx";
+import Journal from "./components/Journal/Journal.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Edit from "./components/Edit/Edit.jsx";
+import NotFound from "./components/NotFound/NotFound.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
-      <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/home' element={<Home/>}/>
-    <Route path='/journal'>
-      <Route index element={<Journal/>}/>
-      <Route path=":id" element={<Edit/>}/>
-      </Route>
-      <Route path='*' element={<NotFound/>}/>
-      </Routes>
-      <Footer/>
+        <Header />
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/home" element={<Home />} />
+          <Route path="/journal">
+            <Route index element={<Journal />} />
+            <Route path=":id" element={<Edit />} />
+          </Route>
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
-    
     </div>
   );
 }
